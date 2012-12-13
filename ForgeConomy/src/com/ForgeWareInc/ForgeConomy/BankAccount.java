5ec7020f1a07a48f31663910311c70ca20c75983
DJ Crocker <mail@djcrocker.com>
2012-12-13 01:41:50 -0500

    Bugfix/comment/enhance BankAccount class
    Fixed bug in addMember() which caused the member to be added multiple
    times regardless of whether they already exist in the ArrayList.
    
    Added javadoc comments to class and all methods
    
    Changed withdraw() to return a boolean indicating success or failure
    
    Added toString() method to return information about the account in the
    format:
    Balance: xxxx
    Owner: xxxxxxx
    Members:
    xxxxx, xxxxx, xxxx, xxxxxxxxx,
    xxx, xxxxxx, xxxxx
    
    Compiles - no errors

--- ForgeConomy/src/com/ForgeWareInc/ForgeConomy/BankAccount.java
+++ ForgeConomy/src/com/ForgeWareInc/ForgeConomy/BankAccount.java
@@ -2,51 +2,113 @@ package com.ForgeWareInc.ForgeConomy;
 
 import java.util.ArrayList;
 
+/**
+	A class representing a bank account that supports multiple members.
+	All normal bank account methods (deposit, withdraw, getBalance, etc.)
+	are present. The account's owner (index 0 in ArrayList of members) has
+	additional privilages to add/remove members and delete the account.
+	Regular members have access to the account's balance (both deposit and
+	withdraw).
+*/ 
 public class BankAccount{
-
+	
 	private ArrayList<String> members = new ArrayList<String>();
 	private int balance = 0;
 	
+	/**
+		Constructor; creates a bank account with an owner and a balance
+		@param user the bank account's owner and primary user
+		@param amount the bank account's initial balance
+	*/
 	public BankAccount(String user, int amount){
 		members.add(user);
 		balance = amount;
 	}
 	
-	public BankAccount() {
-
-	}
-
+	/**
+		Accessor for balance
+		@return the account's current balance
+	*/
 	public int getBalance(){
 		return balance;
 	}
 	
+	/**
+		Accessor for the acount's owner
+		@return the account's owner
+	*/
 	public String getOwner(){
 		return members.get(0);
 	}
 	
-	public ArrayList<String> getMembers(){
+	/**
+		Accessor for members
+		@return an ArrayList containing the account's members
+	*/
+		public ArrayList<String> getMembers(){
 		return members;
 	}
 	
+	/**
+		Deposits funds into the account
+		@param amount the amount to be deposited
+		@return the account's balance after the deposit
+	*/
 	public int deposit(int amount){
 		balance = balance + amount;
 		return balance;
 	}
 	
-	public int withdraw(int amount){
+	/**
+		Withdraws funds from the account if sufficient funds exist
+		@param amount the amount to be withdrawn
+		@return whether the withdraw was successful
+	*/
+	public boolean withdraw(int amount){
 		if(balance >= amount){
 			balance = balance - amount;
+			return true;
+		}else{
+			return false;
 		}
-		return balance;
 	}
 	
+	/**
+		Adds a member to the account
+		@param member the member to be added
+	*/
 	public void addMember(String member){
-		for(String memberName : members){
-			if(!memberName.equals(member)){
-				members.add(member);
-			}else{
-				//already member
-			}
+		if(!members.contains(member)){
+			members.add(member);
+		}else{
+			//already member
 		}
+	}
+	
+	/**
+		Describes the bank account, listing the current
+		balance, owner, and all members
+		@return a String containing information about the account
+	*/
+	public String toString(){
+		String output = ("Balance: "+balance+"\nOwner: "+members.get(0));
+		if(members.size()==1){
+			return output;
+		}
+		output += ("\nMembers:\n\t");
+		// adds members to string in rows of four
+		for(int i=1; i<members.size();){
+			for(int k=0; k<4; k++){
+				if(i==(members.size()-1)){
+					output += members.get(i);
+					return output;
+				}else{
+				output += (members.get(i)+", ");
+				i++;
+				}
+			}
+			output += "\n\t";
+		}
+		return output;
 	}
 }
